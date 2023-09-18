8ebecc979b620622197d2ce086365a54250a6057
acs_isep <acs@isep.ipp.pt>
2023-09-18 16:44:43 +0100

    Changed dependencies in package.json file

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,6 @@ const calc_1 = require("./calc");
 let printMessage = (msg) => console.log(`Mensagem: ${msg}`);
 let msg = "Hello, TypeScript";
 printMessage(msg);
-debugger;
-let total = calc_1.soma(1, 2, 3, 4);
+//debugger;
+let total = (0, calc_1.soma)(1, 2, 3, 4);
 printMessage(total.toString());
